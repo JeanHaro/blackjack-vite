479aba85e701507a1d45d3df03d97a5413cdf1c3
JeanHaro <JeanCHL98@hotmail.com>
2026-03-19 00:57:16 -0500

    92 - Mejorar la documentación JSDoc Comments

--- src/blackjack/usecases/crear-deck.js
+++ src/blackjack/usecases/crear-deck.js
@@ -1,9 +1,20 @@
 import _ from 'underscore';
 
+
+/**
+ * Esta función crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta  Ejemplo: ['C', 'D', 'H', 'S']
+ * @param {Array<String>} tiposEspeciales Ejemplo: ['A', 'J', 'Q', 'K']
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
 // Crea una nueva baraja
 export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
-    let deck = [];
+    // Si no muestra los tipos de carta
+    if (!tiposDeCarta || !tiposDeCarta.length === 0) throw new Error('TiposDeCarta es obligatorio como un arreglo de string');
 
+    if (!tiposEspeciales || !tiposEspeciales.length === 0) throw new Error('TiposDeCarta es obligatorio como un arreglo de string');
+    
+    let deck = [];
 
     for (let i = 2; i <= 10; i++) {
         for (let tipo of tiposDeCarta) {
