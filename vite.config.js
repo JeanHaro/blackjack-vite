616b01bd1b64b632b3f515c99ab36203eba7c67f
JeanHaro <JeanCHL98@hotmail.com>
2026-03-19 17:44:29 -0500

    104 - Desplegar en GitHub Pages

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// Vamos una configuración a las características como viene por defecto Vite
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    base: '/blackjack-vite/' // Para que encuentre los CSS y JS el GitHub Pages en docs
+});
